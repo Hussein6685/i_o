1e7e788012c8a58061bf26b05a9f5e1e6c6e4f90
Hussein6685 <husseinail6685@gmail.com>
2026-06-24 00:27:25 +0300

    Add Community model, migration, fixtures, and test setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_28_082029) do
+ActiveRecord::Schema[8.0].define(version: 2026_06_23_211005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "communities", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
